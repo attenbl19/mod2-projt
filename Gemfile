79adbc1619f14685209ab467215f00b613342cab
attenbl19 <attenboroughl@protonmail.com>
2020-09-07 09:51:31 -0400

    adding bcrypt gem

--- Gemfile
+++ Gemfile
@@ -51,4 +51,8 @@ group :test do
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+# gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem 'faker', '~> 2.13'
+
+gem 'bcrypt', '~> 3.1', '>= 3.1.16'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require faker
+# for address data
+Faker::Address.street_address
+Faker::Address.city
+Faker::Address.state
+Faker::Address.zip_code
+
+#for date and time data
+Faker::Time.between(from: DateTime.now - 1, to: DateTime.now, format: :short)
+
+#names of Technicians
+Faker::Name.name
+
+#for phone numbers
+Faker::PhoneNumber.phone_number
+
+
+
