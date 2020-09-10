c42c717658782848c26bf268c311a0e8d0c648ac
attenbl19 <attenboroughl@protonmail.com>
2020-09-10 10:09:11 -0400

    correcting marcos

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,5 +1,5 @@
 class Client < ApplicationRecord
-    has_many :appointments dependent: :destroy
-    has_many :nail_technicians through: :appointments
+    has_many :appointments, dependent: :destroy
+    has_many :nail_technicians, through: :appointments
     has_secure_password
 end
